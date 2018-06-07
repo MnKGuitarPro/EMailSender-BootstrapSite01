f1ec1fa6ac989e4a904482781db627c5228578cc
Daniel Pavez-Sandoval <daniel.pavez@usach.cl>
2018-06-07 17:42:25 -0400

    First commit

---
+++ js/contact_me.js
@@ -0,0 +1,71 @@
+$(function() {
+
+  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function($form, event, errors) {
+      // additional error messages or events
+    },
+    submitSuccess: function($form, event) {
+      event.preventDefault(); // prevent default submit behaviour
+      // get values from FORM
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var phone = $("input#phone").val();
+      var message = $("textarea#message").val();
+      var firstName = name; // For Success/Failure Message
+      // Check for white space in name for Success/Fail message
+      if (firstName.indexOf(' ') >= 0) {
+        firstName = name.split(' ').slice(0, -1).join(' ');
+      }
+      $this = $("#sendMessageButton");
+      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+      $.ajax({
+        url: "././mail/mail.php",
+        type: "POST",
+        data: {
+          name: name,
+          phone: phone,
+          email: email,
+          message: message
+        },
+        cache: false,
+        success: function() {
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-success')
+            .append("<strong>Su mensaje ha sido enviado. Nos pondremos en contacto lo antes posible.</strong>");
+          $('#success > .alert-success')
+            .append('</div>');
+          $('#contactForm').trigger("reset");
+        },
+        error: function() {
+          $('#success').html("<div class='alert alert-danger'>");
+          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-danger').append('</div>');
+          $('#success > .alert-danger').append($("<strong>").text("Lo sentimos " + firstName + ", no pudimos entregar su E-Mail. Por favor, intente de nuevo más tarde. Si lo desea, puede enviarnos un EMail desde su proveedor de cuentas de correo (GMail, HotMail, OutLook, etc.) a contacto@ceimchile.com"));
+          $('#contactForm').trigger("reset");
+        },
+        complete: function() {
+          setTimeout(function() {
+            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+          }, 1000);
+        }
+      });
+    },
+    filter: function() {
+      return $(this).is(":visible");
+    },
+  });
+
+  $("a[data-toggle=\"tab\"]").click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+  $('#success').html('');
+});
---
+++ mail/mail.php
@@ -0,0 +1,55 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+require '../vendor/autoload.php';
+
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+   return false;
+   }
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPDebug = 2;
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPSecure = 'tls';
+$mail->SMTPAuth = true;
+
+$mail->Username = "<your.email.sender>@gmail.com";
+$mail->Password = "<password.email.sender>";
+$mail->setFrom('<your.email.sender>@gmail.com',$name);
+$mail->addAddress('contacto@<your.site>.com', '<site admin>');
+
+$mail->Subject = 'Mail enviado de <your site>';
+$mail->Body    = "Ha recibido un nuevo mensaje desde el formulario de contacto del Sitio Web.\n\n"."Aquí están los detalles:\n\nNombre: $name\n ----- Email: $email_address\n ----- Teléfono: $phone\n ----- Mensaje: $message";
+$mail->AltBody = "Ha recibido un nuevo mensaje desde el formulario de contacto del Sitio Web.\n\n"."Aquí están los detalles:\n\nNombre: $name\n ----- Email: $email_address\n ----- Teléfono: $phone\n ----- Mensaje: $message";
+
+if ($mail->send()) {
+    if (save_mail($mail)) {
+        return true;
+    } else {
+      return false;
+    }
+} else {
+    return false;
+}
+
+function save_mail($mail)
+{
+    $path = "{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail";
+    //Tell your server to open an IMAP connection using the same username and password as you used for SMTP
+    $imapStream = imap_open($path, $mail->Username, $mail->Password);
+    $result = imap_append($imapStream, $path, $mail->getSentMIMEMessage());
+    imap_close($imapStream);
+    return $result;
+}
